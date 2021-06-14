5d24869ae8449976f0aef6aa25f4e310da4f0c2d
Joakim JFKK. Krugstrup <33222649+TimTheFiend@users.noreply.github.com>
2021-06-14 12:29:45 +0200

    Database init

---
+++ Models/Elev.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GFElevInterview.Models
+{
+    public class Elev
+    {
+        [Key]
+        public int CprNr { get; set; }
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+    }
+}
