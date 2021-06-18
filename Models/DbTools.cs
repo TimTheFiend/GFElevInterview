b2a5a69dcdf38ee3d1cc5727de1e4024522ac8d4
Afsaneh Bajoori <afsan.bajoori@hotmail.com>
2021-06-18 10:05:17 +0200

    last changes

--- Models/DbTools.cs
+++ Models/DbTools.cs
@@ -25,19 +25,19 @@ namespace GFElevInterview.Models
                 new ElevModel { CprNr = 1111931234, Fornavn = "Joakim", Efternavn = "Krugstrup" }, 
                 new ElevModel { CprNr = 0101954321, Fornavn = "Peder", Efternavn = "Eriksen" }, 
                 new ElevModel { CprNr = 1202341233, Fornavn = "Søm", Efternavn = "Johammer" } ,
-                new ElevModel { CprNr = 1151931234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1181931234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1191931234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1141931234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1152931234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1181231234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1191951234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1141901234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1141908234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1131901234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1141901934, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1101901234, Fornavn = "Joakim", Efternavn = "Krugstrup" },
-                new ElevModel { CprNr = 1101901934, Fornavn = "Joakim", Efternavn = "Krugstrup" }
+                new ElevModel { CprNr = 1151931234, Fornavn = "Joakim13", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1181931234, Fornavn = "Joakim12", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1191931234, Fornavn = "Joakim11", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1141931234, Fornavn = "Joakim10", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1152931234, Fornavn = "Joakim9", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1181231234, Fornavn = "Joakim8", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1191951234, Fornavn = "Joakim7", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1141901234, Fornavn = "Joakim6", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1141908234, Fornavn = "Joakim5", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1131901234, Fornavn = "Joakim4", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1141901934, Fornavn = "Joakim3", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1101901234, Fornavn = "Joakim2", Efternavn = "Krugstrup" },
+                new ElevModel { CprNr = 1101901934, Fornavn = "Joakim1", Efternavn = "Krugstrup" }
 
             );
         }
