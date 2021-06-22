d5dbad6841f70ab79a51184ca4b325477fcff894
Victor Gawron <viga@pc-tec.local>
2021-06-22 12:00:01 +0200

    Ændring i PDF navne

---
+++ Data/BlanketUdskrivning.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GFElevInterview.Data
+{
+    public class BlanketUdskrivning
+    {
+
+        public static bool UdskrivningRKV()
+        {
+            string inputFil = GetFile(); 
+
+            
+            return false;
+        }
+
+        private static string GetFile()
+        {
+            string foo = "EUV1 - Infrastruktur.pdf";
+            return "Blanketter\\Templates\\" + foo;
+        }
+    }
+}
