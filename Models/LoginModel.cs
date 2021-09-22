c52c3d968090f41843c8fd5c7319d641b0258725
Victor Gawron <victorgawron@yahoo.dk>
2021-09-22 09:36:54 +0200

    Login form start up

---
+++ Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GFElevInterview.Models
+{
+    class LoginModel
+    {
+        [Key]
+        public int id { get; set; }
+
+        //-brugernavn
+        public string brugernavn { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
---
+++ Views/LoginView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GFElevInterview.Views
+{
+    /// <summary>
+    /// Interaction logic for LogindView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        private static readonly string _password = "root";
+
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
